1c4e3e487c7b0d0aa686a0e2a7f59e2ce977949e
heqinming <516001539@qq.com>
2020-06-03 21:58:22 +0800

    filter中使用匿名函数lambda

--- demo27_lambda.py
+++ demo27_lambda.py
@@ -1,4 +1,9 @@
-# 定位是
-
+# 定位是指定匿名函数的
 func = lambda x:x+1
-print(func)
+print(func(3))
+test = lambda x,y:x>y
+print(test(4,3))
+# python支持一个方法filter
+myList = [1,2,3,4,5,6,7,8,9]
+result = filter(lambda x:x%2==0, myList)
+print(list(result))
