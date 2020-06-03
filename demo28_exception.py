1f1537d7441426b6659c1bfbffee7ea58d9465be
heqinming <516001539@qq.com>
2020-06-03 22:16:04 +0800

    事件捕获

--- demo28_exception.py
+++ demo28_exception.py
@@ -7,3 +7,23 @@ except NameError:
     print("调用了未定义变量")
 finally:
     print("exit")
+
+
+
+# 如何捕获多个错误
+try:
+    age = int(input("请输入年龄：")) 
+    print(age)
+    print(count)
+
+# 方案二
+except ValueError:
+    print("输入的数值不符合要求")
+except NameError:
+    print("调用了不存在变量")
+# 方案一 
+'''
+except Exception as msg:
+    print(msg)
+    print("程序出错了")
+'''
