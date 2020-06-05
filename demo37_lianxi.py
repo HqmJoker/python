660d4a765ef25cc3872a955329cf65a411e67fad
HqmJoker <516001539@qq.com>
2020-06-05 22:00:04 +0800

    如果父类构造函数和子类一样，子类可省略不写

--- demo37_lianxi.py
+++ demo37_lianxi.py
@@ -10,8 +10,8 @@ class Student:
         print("学号为%s的学生%s正在上课!"%(self.id, self.name))
 
 class Doctor(Student):
-    def __init__(self, id, name):
-        super().__init__(id, name)
+    # def __init__(self, id, name):
+        # super().__init__(id, name)
     def publish(self):
         print("%s发布了一篇论文"%(self.name))
 
