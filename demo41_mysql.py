a016d0b34348bb6dc801b185e234232271a2cdd8
HqmJoker <516001539@qq.com>
2020-06-10 22:22:20 +0800

    上传数据库示例

--- demo41_mysql.py
+++ demo41_mysql.py
@@ -5,10 +5,12 @@ myConnection = conn.connect(host="localhost", user="root", passwd="", database="
 # 创建游标对象(指向服务器端查询结果集合中某个记录位置的变量)
 myCursor = myConnection.cursor(dictionary=True)
 # 执行sql
-sql = "select * from musicgu_user limit %s,%s"
+sql = "select * from music_user limit %s,%s"
 myCursor.execute(sql, (1,1))
 # sql = "insert into music_user (name, password) values ('joker', '123456')"
 # myCursor.execute(sql)
 # myConnection.commit()
+# myCursor.rowcount   受影响行数
+# myCursor.lastrowid  最后操作id
 result = myCursor.fetchall()
 print(result)
