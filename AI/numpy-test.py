419099c82454a2de67334f9e26c37c3e44e08d56
HqmJoker <44420292+HqmJoker@users.noreply.github.com>
2020-10-13 09:34:54 +0800

    调试代码

--- AI/numpy-test.py
+++ AI/numpy-test.py
@@ -6,4 +6,7 @@ print(arr2)
 print(arr+arr2)
 print(arr * arr2)
 print(arr)
-print(arr.shape)
+print(arr.shape) #输出结果表示当前数组为一维数组，有5个元素
+
+#target_list = [1,2,3,4,5]
+#target_list.shape() 普通数组无法使用shape函数
