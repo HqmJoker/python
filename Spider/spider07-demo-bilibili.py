7480828543cc95e0423ed153f778189e98e7a89f
HqmJoker <44420292+HqmJoker@users.noreply.github.com>
2020-10-19 17:44:41 +0800

    上传最新版代码（未完成）

--- Spider/spider07-demo-bilibili.py
+++ Spider/spider07-demo-bilibili.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait as wdWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 browser = webdriver.Chrome()
 WAIT = wdWait(browser, 10)
@@ -14,8 +15,29 @@ browser.execute_script(js)
 
 input = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#nav_searchform > input')))
 submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//form[@id="nav_searchform"]//button')))
-# submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="nav_searchform"]//div[@class="nav-search-btn"]//button')))
-# submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/div/div[2]/div/form/div/button')))
 
 input.send_keys('蔡徐坤 篮球')
 submit.click()
+
+print('跳转到新窗口')
+all_h = browser.window_handles
+browser.switch_to.window(all_h[1])
+total = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#server-search-app > div.contain > div.body-contain > div > div.page-wrap > div > ul > li.page-item.last > button')))
+for i in range(2, int(total + 1)):
+    next_page(i)
+
+def next_page(page_num):
+    print('获取下一页数据')
+    try:
+        next_btn = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#server-search-app > div.contain > div.body-contain > div > div.page-wrap > div > ul > li.page-item.next > button')))
+        next_btn.click()
+        WAIT.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#server-search-app > div.contain > div.body-contain > div > div.page-wrap > div > ul > li.page-item.active > button'), str(page_num)))
+    except TimeoutException:
+        browser.refresh()
+        return next_page(page_num) 
+
+
+def get_source():
+    print(21111)
+
+    
